53d4351e1becad985201afc51234d6dd6c131d06
Antti Kupila <akupila@gmail.com>
2019-02-16 15:12:17 +0100

    resource: add source code to request

--- resource/request.go
+++ resource/request.go
@@ -1,6 +1,9 @@
 package resource
 
 import (
+	"context"
+	"io"
+
 	"github.com/aws/aws-sdk-go-v2/aws"
 )
 
@@ -10,10 +13,24 @@ type AuthProvider interface {
 	AWS() (aws.CredentialsProvider, error)
 }
 
+// SourceCode contains one set of source code, matching a single source entry
+// for the resource.
+type SourceCode interface {
+	// Digest returns a hash digest.
+	Digest() string
+
+	// Size returns the size of the source tarball in bytes.
+	Size() int
+
+	// Reader returns a reader to the source tarball.
+	Reader(ctx context.Context) (targz io.ReadCloser, err error)
+}
+
 // A CreateRequest is passed to a resource's Create method when a new resource
 // is being created.
 type CreateRequest struct {
-	Auth AuthProvider
+	Auth   AuthProvider
+	Source []SourceCode
 }
 
 // An UpdateRequest is passed to a resource's Update method when a new resource
@@ -23,6 +40,7 @@ type CreateRequest struct {
 // Previous will match the resource type.
 type UpdateRequest struct {
 	Auth     AuthProvider
+	Source   []SourceCode
 	Previous interface{}
 }
 
