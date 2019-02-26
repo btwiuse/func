495159c43931c7b23724bc3b5f4fa70864c7a2e6
Antti Kupila <akupila@gmail.com>
2019-02-26 17:23:45 +0100

    cmd: register providers in provider package

---
+++ provider/aws/register.go
@@ -0,0 +1,16 @@
+package aws
+
+import (
+	"github.com/func/func/resource"
+)
+
+type registry interface {
+	Register(resource.Definition)
+}
+
+// Register adds all supported AWS resources to the registry.
+func Register(reg registry) {
+	reg.Register(&LambdaFunction{})
+	reg.Register(&IAMRole{})
+	reg.Register(&APIGatewayRestAPI{})
+}
