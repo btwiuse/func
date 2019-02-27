ed81123a4108fa831da28c842fafb8558221933f
Antti Kupila <akupila@gmail.com>
2019-02-27 15:15:21 +0100

    graph: support blocks in decoder
    Add support for decoding a nested block, such as:
    
      resource "aws_apigateway_rest_api" "api" {
        endpoint_configuration {
          types = ["EDGE"]
        }
      }
    
    Unlike hcl2, only one block is supported.

--- graph/decoder/io.go
+++ graph/decoder/io.go
@@ -1,6 +1,7 @@
 package decoder
 
 import (
+	"fmt"
 	"reflect"
 
 	"github.com/func/func/resource"
@@ -25,7 +26,13 @@ func decodeInput(val reflect.Value, config hcl.Body) (vals map[string]cty.Value,
 	}
 
 	vals = make(map[string]cty.Value)
+
+	// Attributes
 	for _, input := range inputs {
+		if isBlock(input) {
+			continue
+		}
+
 		attr, ok := cont.Attributes[input.Name]
 		if !ok {
 			// Optional attribute was not set
@@ -55,6 +62,68 @@ func decodeInput(val reflect.Value, config hcl.Body) (vals map[string]cty.Value,
 		diags = append(diags, morediags...)
 	}
 
+	// Blocks
+	blocksByType := cont.Blocks.ByType()
+	for _, input := range inputs {
+		if !isBlock(input) {
+			continue
+		}
+		blocks := blocksByType[input.Name]
+
+		if len(blocks) == 0 {
+			if input.Type.Kind() == reflect.Ptr {
+				// Missing optional block
+				continue
+			}
+			// Missing required block
+			diags = append(diags, &hcl.Diagnostic{
+				Severity: hcl.DiagError,
+				Summary:  fmt.Sprintf("Missing %s block", input.Name),
+				Detail:   fmt.Sprintf("A %s block is required.", input.Name),
+				Subject:  config.MissingItemRange().Ptr(),
+			})
+			continue
+		}
+		if len(blocks) > 1 {
+			diags = append(diags, &hcl.Diagnostic{
+				Severity: hcl.DiagError,
+				Summary:  fmt.Sprintf("Duplicate %s block", input.Name),
+				Detail: fmt.Sprintf(
+					"Only one %s block is allowed. Another was defined on line %d",
+					input.Name,
+					blocks[0].DefRange.Start.Line,
+				),
+				Subject: blocks[1].DefRange.Ptr(),
+			})
+			continue
+		}
+
+		if input.Type.Kind() == reflect.Ptr {
+			// Initialize struct
+			v := reflect.New(input.Type.Elem())
+			val.Field(input.Index).Set(v)
+		}
+
+		field := reflect.Indirect(val.Field(input.Index))
+
+		if !field.IsValid() {
+			diags = append(diags, &hcl.Diagnostic{
+				Severity: hcl.DiagError,
+				Summary:  fmt.Sprintf("huh"),
+				Subject:  blocks[0].DefRange.Ptr(),
+			})
+			continue
+		}
+
+		block := blocks[0]
+		kv, r, d := decodeInput(field, block.Body)
+		for k, v := range kv {
+			vals[k] = v
+		}
+		refs = append(refs, r...)
+		diags = append(diags, d...)
+	}
+
 	return vals, refs, diags
 }
 
@@ -64,9 +133,27 @@ type ref struct {
 	field resource.Field
 }
 
+func isBlock(f resource.Field) bool {
+	if f.Type.Kind() == reflect.Struct {
+		// Required block
+		return true
+	}
+	if f.Type.Kind() == reflect.Ptr && f.Type.Elem().Kind() == reflect.Struct {
+		// Optional block
+		return true
+	}
+	return f.Type.Kind() == reflect.Struct
+}
+
 func inputSchema(ff []resource.Field) *hcl.BodySchema {
 	schema := &hcl.BodySchema{}
 	for _, f := range ff {
+		if isBlock(f) {
+			schema.Blocks = append(schema.Blocks, hcl.BlockHeaderSchema{
+				Type: f.Name,
+			})
+			continue
+		}
 		schema.Attributes = append(schema.Attributes, hcl.AttributeSchema{
 			Name:     f.Name,
 			Required: f.Type.Kind() != reflect.Ptr,
